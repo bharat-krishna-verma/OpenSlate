d5ef6418a95bf0ca966b462044e6d0810c6eb5b0
sparenamecooldude69 <108430804+sparenamecooldude69@users.noreply.github.com>
2025-07-25 00:41:11 +0530

    updating

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,9 @@
 import React, { use, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useState } from 'react'
-import { blog_data } from '../assets/assets';
+import { assets, blog_data } from '../assets/assets';
 import Navbar from '../components/Navbar';
-
+import Moment from 'moment';
 const Blog = () => {
   const {id}= useParams();
   const [data,setData]= useState(null);
@@ -14,10 +14,16 @@ const Blog = () => {
   useEffect(()=>{
     fetchBlogData()},[])
   return data ?(
-    <div>
-    <Navbar></Navbar>
-      <div>
-
+    <div className='relative'>
+    <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50'/>
+    <Navbar/>
+      <div className='text-center mt -20 text-gray-600'>
+        <p className='text-primary py-4 font-medium'>Published on 
+            {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
+        <h1 className='text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800'>{data.title}</h1>
+        <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
+        <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm 
+        border-primary/35 bg-primary/5 font-medium text-primary'>Michael Brown</p>
       </div>
       <div>
 
