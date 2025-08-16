d21346cd5a734cd536ed8803bf9d2842cca365a3
sparenamecooldude69 <108430804+sparenamecooldude69@users.noreply.github.com>
2025-08-16 09:37:37 +0530

    added new files

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -1,3 +1,7 @@
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js';
+import Comment from '../models/Comment.js';
 export const addBlog=async (req,res)=>{
     try{
         const {title,subTitle,description,category,isPublished}=JSON.parse(req.body.blog);
@@ -6,8 +10,84 @@ export const addBlog=async (req,res)=>{
         if(!title||!description||!category||!imageFile){
             return res.json({success:false,message:"Missing required fields"})
         }
+        const fileBuffer=fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder:"/blogs"
+        })
+        const optimizedIMageUrl = imagekit.url({
+            path: response.filePath,
+            transformation:[
+                {quality:'auto'},
+                {format: 'webp'},
+                {width:'1280'}
+            ]
+        })
+        const image = optimizedIMageUrl;
+        await Blog.create({title,subTitle,description,category,image,isPublished})
+        res.json({success: true,message:"Blog added successfully"})
+
         
     }catch(error){
-
+         res.json({success: false,message:error.message})
+    }
+}
+export const getAllBlogs= async()=>{
+    try{
+        const blogs= await Blog.find({isPublished:true})
+        res.json({success: true,blogs})
+    }catch(error){
+         res.json({success: false,message: error.message})
+    }
+}
+export const getBlogById= async(req,res)=>{
+    try {
+        const {blogId}= req.parse;
+        const blog = await Blog.findById(blogId)
+        if(!blog){
+            return res.json({success: false,message:"Blog not found"});
+        }
+         res.json({success: true,blog})
+    } catch (error) {
+         res.json({success: false,message: error.message})
+    }
+}
+export const deleteBlogById= async(req,res)=>{
+    try {
+        const {id}= req.body;
+        await Blog.findByIdAndDelete(id);
+         res.json({success: true,message:"Blog Deleted successfully"})
+    } catch (error) {
+         res.json({success: false,message: error.message})
+    }
+}
+export const togglePublish = async(req,res)=>{
+    try {
+        const {id} = req.body;
+        const blog = await Blog.findById(id);
+        blog.isPublished=!blog.isPublished;
+        await blog.save();
+        res.json({success: true,message:"Blog status updated"})
+    } catch (error) {
+        res.json({success: false,message: error.message})
+    }
+}
+export const addComment = async(req,res)=>{
+    try {
+        const {blog,name,content}= req.body;
+        await Comment.create({blog,name,content});
+        res.json({success: true,message: 'Comment added for review'})
+    } catch (error) {
+        res.json({success: false,message: error.message})
+    }
+}
+export const getBlogComments = async(req,res)=>{
+    try {
+        const {blogId}= req.body;
+        const comments= await Comment.find({blog:blogId,isApproved:true}).sort({createdAt:-1});
+        res.json({success: true,comments})
+    } catch (error) {
+        res.json({success: false,message: error.message})
     }
 }
--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ import cors from 'cors'
 import { connect } from 'mongoose';
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
 const app = express();
 await connectDB
 app.use(cors());
 app.use(express.json());
 app.get('/',(req,res)=>res.send("API is Working"))
 app.use('/api/admin',adminRouter)
+app.use('/api/blod',blogRouter)
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
     console.log("Server is running on "+ PORT)
