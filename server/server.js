831abef06fbc30a4ea1c7d02136644ea969d90ff
sparenamecooldude69 <108430804+sparenamecooldude69@users.noreply.github.com>
2025-08-19 23:50:32 +0530

    debugged

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
 import blogRouter from './routes/blogRoutes.js';
 const app = express();
-await connectDB
+await connectDB();
 app.use(cors({
   origin: 'https://open-slate.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
