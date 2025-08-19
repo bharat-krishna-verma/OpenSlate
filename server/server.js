fb6f9004703a135339dac89d8e252c68f58771fe
sparenamecooldude69 <108430804+sparenamecooldude69@users.noreply.github.com>
2025-08-19 23:39:54 +0530

    made changes

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.get('/',(req,res)=>res.send("API is Working"))
 app.use('/api/admin',adminRouter)
-app.use('/api/blod',blogRouter)
+app.use('/api/blog',blogRouter)
 const PORT=process.env.PORT||3000;
 app.listen(PORT,()=>{
     console.log("Server is running on "+ PORT)
